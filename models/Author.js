d059cac8111e10d7767f2061230c31bb405186a7
JuanCVargasC <juancamilo.vargas@uptc.edu.co>
2023-07-18 14:53:51 +0100

    3rd change

---
+++ models/Author.js
@@ -0,0 +1,28 @@
+const mongoosee = require('mongoose')
+
+const { Schema } = mongoosee
+
+const AuthorSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true        
+    },
+    birthay: {
+        type: Date,
+        required: false
+    },
+    song: [
+        {
+            type: Schema.Types.ObjectId,//alamacena id obejto
+            ref: song
+        }
+    ]
+
+})
+
+module.exports = mongoosee.model('author',AuthorSchema)
---
+++ models/Song.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const {Schema} = mongoose
+
+const  SongSchema =({
+    number: {
+        type: Number,
+        required: true
+    },
+    tittle: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: author
+    }
+})
+
+module.exports = mongoose.model('song',SongSchema)
